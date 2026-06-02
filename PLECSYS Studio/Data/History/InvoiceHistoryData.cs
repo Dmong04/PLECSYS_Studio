39e58ddb5fcf4df39acee53604b4301427145984
Pinaruma <pinasanthony88@gmail.com>
2026-06-02 12:02:37 -0600

    Vista historico
    Funcionando, se cambió la vista

--- PLECSYS Studio/Data/History/InvoiceHistoryData.cs
+++ PLECSYS Studio/Data/History/InvoiceHistoryData.cs
@@ -1,26 +1,33 @@
-﻿using PLECSYS_Studio.Wrappers;
+﻿using PLECSYS_Studio.Services;
+using PLECSYS_Studio.Wrappers;
 using PLECSYS_Studio.Wrappers.History;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace PLECSYS_Studio.Data.History
 {
-    public class InvoiceHistoryData(IHttpClientFactory factory)
+    public class InvoiceHistoryData(IHttpClientFactory factory, SessionService session)
     {
         private readonly HttpClient _http = factory.CreateClient("PLECSYS");
 
         public async Task<APIResponse<List<InvoiceHistoryResponse>>> GetInvoiceHistoryByUserAndCompanyId(FindHistoryRequest request)
         {
-            var response = await _http.PostAsJsonAsync("invoice/history/all", request);
+            var token = session.GetAccessToken();
+
+            using var httpRequest = new HttpRequestMessage(HttpMethod.Post, "invoice/history/all");
+            httpRequest.Content = new StringContent(
+                JsonSerializer.Serialize(request),
+                Encoding.UTF8,
+                "application/json");
+            httpRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _http.SendAsync(httpRequest);
             response.EnsureSuccessStatusCode();
 
-            var success = await response.Content.ReadFromJsonAsync<APIResponse<List<InvoiceHistoryResponse>>>();
-            return success;
+            var result = await response.Content.ReadFromJsonAsync<APIResponse<List<InvoiceHistoryResponse>>>();
+            return result ?? new APIResponse<List<InvoiceHistoryResponse>> { Data = null, Success = false, Message = "Respuesta vacía" };
         }
     }
 }
